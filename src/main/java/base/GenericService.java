1f024c3249364fccdcbd1218876cb9cddfe038a4
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2015-12-21 10:13:20 +0530

    Testing git  command line

---
+++ src/main/java/base/GenericService.java
@@ -0,0 +1,12 @@
+/* COPYRIGHT (C) 2015 DCHQ. All Rights Reserved. */
+package base;
+
+/**
+ * Generic Service Interface.
+ *
+ *
+ * @author Mohammed Shoukath Ali
+ */
+public interface GenericService {
+
+}
