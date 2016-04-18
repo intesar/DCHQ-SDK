6032245c12fa2cf83b041f57d3d263004ef14113
abedeen <s.z.abedeen@gmail.com>
2016-04-18 10:48:36 +0530

    Create Mechine, Datacenter Search.

---
+++ src/test/java/io/dchq/sdk/core/DataCenterEntitledServiceTest.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.dchq.sdk.core;
+
+import com.dchq.schema.beans.base.Message;
+import com.dchq.schema.beans.base.ResponseEntity;
+import com.dchq.schema.beans.one.base.NameEntityBase;
+import com.dchq.schema.beans.one.base.UsernameEntityBase;
+import com.dchq.schema.beans.one.plugin.Plugin;
+import com.dchq.schema.beans.one.provider.DataCenter;
+import com.dchq.schema.beans.one.security.EntitlementType;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static junit.framework.Assert.assertNull;
+import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by Abedeen on 04/05/16.
+ */
+/**
+ * Abstracts class for holding test credentials.
+ *
+ * @author Abedeen.
+ * @since 1.0
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(Parameterized.class)
+public class DataCenterEntitledServiceTest extends AbstractServiceTest{
+
+    private DataCenterService dataCenterService;
+    private DataCenterService dataCenterService2;
+
+    @org.junit.Before
+    public void setUp() throws Exception{
+        dataCenterService = ServiceFactory.buildDataCenterService(rootUrl, username, password);
+        dataCenterService2 = ServiceFactory.buildDataCenterService(rootUrl, username2, password2);
+    }
+
+    DataCenter dataCenter;
+    DataCenter dataCenterCreated;
+    boolean createError;
+    String validationMessage;
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"Test Cluster - AA4",Boolean.FALSE,EntitlementType.CUSTOM, true, userId2,"\nAll Input Values are normal. Malfunction in SDK",false},
+                {"Test Cluster - AA40",Boolean.FALSE,EntitlementType.CUSTOM, false, USER_GROUP,"\n Auto Flag is set to true ,\n Next required value :Machine Compose id cannot be empty", false},
+                {"Testcluster Entitled ",Boolean.FALSE,EntitlementType.CUSTOM, false, null,"\n Empty Cluster Name is Not Valid", false},
+                {"Testcluster Entitled ",Boolean.FALSE,EntitlementType.CUSTOM, false, "", "\n Empty Cluster Name is Not Valid", false}
+           /*     {"TestPlugin1111", "1.1", "Dummy Script", "PERL", "Apache License 2.0", EntitlementType.CUSTOM, true, userId2, "General Input", false},
+                {"TestPlugin11111", "1.1", "Dummy Script", "PERL", "Apache License 2.0", EntitlementType.CUSTOM, false, USER_GROUP, "General Input", false},
+                {"TestPlugin2", "1.1", "Dummy Script", "PERL", "EULA", EntitlementType.OWNER, false, null, "General Input", false},
+                {"TestPlugin3", "1.1", "Dummy Script", "PERL", "EULA", EntitlementType.OWNER, false, "", "General Input", false}*/
+        });
+    }
+    // Create - Update - Delete
+
+    public DataCenterEntitledServiceTest(String clusterName,Boolean autoScaleFlag,EntitlementType blueprintType, boolean isEntitlementTypeUser, String entitledUserId ,String validationMessage,boolean success) {
+        this.dataCenter = new DataCenter().withName(clusterName).withAutoScale(autoScaleFlag).withBlueprintEntitlementType(blueprintType);
+        if (!StringUtils.isEmpty(entitledUserId) && isEntitlementTypeUser) {
+            UsernameEntityBase entitledUser = new UsernameEntityBase().withId(entitledUserId);
+            List<UsernameEntityBase> entiledUsers = new ArrayList<>();
+            entiledUsers.add(entitledUser);
+            this.dataCenter.setEntitledUsers(entiledUsers);
+        } else if (!StringUtils.isEmpty(entitledUserId)) { // assume user-group
+            NameEntityBase entitledUser = new NameEntityBase().withId(entitledUserId);
+            List<NameEntityBase> entiledUsers = new ArrayList<>();
+            entiledUsers.add(entitledUser);
+            this.dataCenter.setEntitledUserGroups(entiledUsers);
+        }
+        this.createError=success;
+        this.validationMessage=validationMessage;
+
+    }
+
+    @org.junit.Test
+    public void testEntitle() throws Exception {
+
+        logger.info("Create Cluster with Name [{}]", dataCenter.getName());
+        if (createError)
+            logger.info("Input for create Cluster is Expected to generate Error: [{}]", validationMessage);
+
+        ResponseEntity<DataCenter> response = dataCenterService.create(dataCenter);
+        for (Message message : response.getMessages())
+            logger.warn("Error while Create request  [{}] ", message.getMessageText());
+
+        if (response.getResults() != null) this.dataCenterCreated = response.getResults();
+
+        assertNotNull(response);
+        assertNotNull(response.isErrors());
+        assertEquals(validationMessage, ((Boolean) createError).toString(), ((Boolean) response.isErrors()).toString());
+
+        //if(!response.isErrors()  )
+
+        if (!createError) {
+
+            assertNotNull(response.getResults());
+            assertNotNull(response.getResults().getId());
+
+            this.dataCenterCreated = response.getResults();
+
+            assertEquals(dataCenter.getName(), dataCenterCreated.getName());
+            assertEquals(dataCenter.getEntitledBlueprint(), dataCenterCreated.getEntitledBlueprint());
+            assertEquals(dataCenter.isAutoScale(), dataCenterCreated.isAutoScale());
+
+            // valid User2 can access plugin
+            if (dataCenter.getEntitlementType() == EntitlementType.CUSTOM && !StringUtils.isEmpty(userId2)) {
+                ResponseEntity<DataCenter> entitledResponse = dataCenterService2.findById(dataCenterCreated.getId());
+
+                assertNotNull(entitledResponse);
+                assertNotNull(entitledResponse.isErrors());
+                Assert.assertThat(false, is(equals(entitledResponse.isErrors())));
+
+                assertNotNull(entitledResponse.getResults());
+
+                junit.framework.Assert.assertEquals(dataCenterCreated.getId(), entitledResponse.getResults().getId());
+            } else if (dataCenter.getEntitlementType() == EntitlementType.OWNER) {
+                ResponseEntity<DataCenter> entitledResponse = dataCenterService2.findById(dataCenterCreated.getId());
+                assertNotNull(entitledResponse);
+                assertNotNull(entitledResponse.isErrors());
+                //Assert.assertThat(true, is(equals(entitledResponse.isErrors())));
+                assertNull(entitledResponse.getResults());
+
+            }
+
+
+        }
+    }
+    @After
+    public void cleanUp() {
+        logger.info("cleaning up...");
+
+        if (dataCenterCreated!=null) {
+            dataCenterService.delete(dataCenterCreated.getId());
+        }
+    }
+}
--- src/test/java/io/dchq/sdk/core/DockerServerCreateServiceTest.java
+++ src/test/java/io/dchq/sdk/core/DockerServerCreateServiceTest.java
@@ -15,7 +15,9 @@
  */
 package io.dchq.sdk.core;
 
+import com.dchq.schema.beans.base.Message;
 import com.dchq.schema.beans.base.ResponseEntity;
+import com.dchq.schema.beans.one.blueprint.RegistryAccount;
 import com.dchq.schema.beans.one.provider.DataCenter;
 import com.dchq.schema.beans.one.provider.DockerServer;
 import com.dchq.schema.beans.one.security.EntitlementType;
@@ -27,8 +29,12 @@ import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -49,57 +55,115 @@ public class DockerServerCreateServiceTest extends AbstractServiceTest {
 
     @org.junit.Before
     public void setUp() throws Exception {
-        dockerServerService = ServiceFactory.buildDockerServerService(rootUrl, username, password);
+        dockerServerService = ServiceFactory.buildDockerServerService(rootUrl2, username, password);
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {"Test RACKSPACE SERVER ", Boolean.FALSE, "DFW", "general1-1", "DFW/6d833af9-9e31-4d4e-a4ea-1f7f3a4d4406", 1, "2c918086531095990153e05c4ac30f24", "RACKSPACE", false},
+                {"Test_RACKSPACE_SERVER2 ", Boolean.FALSE, "DFW", "general1-1", "DFW/6d833af9-9e31-4d4e-a4ea-1f7f3a4d4406", 1, "2c918086531095990153e05c4ac30f24", "RACKSPACE",400000, false},
 
         });
     }
     // Create - Update - Delete
 
-    public DockerServerCreateServiceTest(String serverName, Boolean activeFlag, String region, String hardwareID, String image, int size, String endpoint, String endpointTpe, boolean success) {
-        this.dockerServer = new DockerServer().withDatacenter(getDataCenter("Test Cluster - AAA", Boolean.FALSE, EntitlementType.ALL_BLUEPRINTS)).withName(serverName)
+    public DockerServerCreateServiceTest(String serverName, Boolean activeFlag, String region, String hardwareID, String image, int size, String endpoint, String endpointTpe, int tinout,boolean success) {
+        this.dockerServer = new DockerServer().withDatacenter(getDataCenter("Test_Cluster_"+(new Date().toString()), Boolean.FALSE, EntitlementType.ALL_BLUEPRINTS)).withName(serverName)
                 .withInactive(activeFlag).withRegion(region).withImageId(image).withSize(size).withEndpoint(endpoint).withEndpointType(endpointTpe).withHardwareId(hardwareID);
+        maxWait=tinout;
+
 
         this.success = success;
     }
 
     public DataCenter getDataCenter(String datacenterName, boolean autoScale, EntitlementType entitlementType) {
         logger.info("Create Cluster with Name [{}]", datacenterName);
-        this.dataCenterService = ServiceFactory.buildDataCenterService(rootUrl, username, password);
+        this.dataCenterService = ServiceFactory.buildDataCenterService(rootUrl2, username, password);
         DataCenter dt = new DataCenter().withName(datacenterName).withAutoScale(autoScale).withBlueprintEntitlementType(entitlementType);
         ResponseEntity<DataCenter> responseEntity = dataCenterService.create(dt);
         if (responseEntity.isErrors())
             logger.warn("Message from Server... {}", responseEntity.getMessages().get(0).getMessageText());
         assertEquals(false, responseEntity.isErrors());
-
         return responseEntity.getResults();
+
+
+      //  return dataCenterService.findById("2c9180865310959901541d78a7fd3322").getResults();
     }
 
     DockerServer dockerServer;
-    DockerServer dockerServerCreated;
+    DockerServer dockerServerCreated,dockerServerProvisioning;
     DataCenterService dataCenterService;
     boolean success;
-
+    ResponseEntity<List<DockerServer>> dockerServerResponseEntity ;
+    String serverID;
+    int maxWait;
     @org.junit.Test
     public void testCreate() throws Exception {
         logger.info("Create Machine with Name [{}]", dockerServer.getName());
         ResponseEntity<DockerServer> response = dockerServerService.create(dockerServer);
+        String errorMessage = "";
+        for (Message message : response.getMessages()) {
+            logger.warn("Error while Create request  [{}] ", message.getMessageText());
+        }
 
-        if (response.isErrors())
-            logger.warn("Message from Server... {}", response.getMessages().get(0).getMessageText());
+
 
         if (response.getTotalElements() == null) {
             logger.info("No Response for  Machine [{}]", dockerServer.getName());
-            assertNotNull(response.getTotalElements());
-        } else if (!success) {
 
-            assertNotNull(response.getResults());
-            assertNotNull(response.getResults().getId());
+//            assertNotNull(response.getTotalElements());
+            Thread.sleep(7000);
+             dockerServerResponseEntity = dockerServerService.search(dockerServer.getName(), 0, 1);
+            errorMessage = "";
+            for (Message message : dockerServerResponseEntity.getMessages()) {
+                logger.warn("Error while Create request  [{}] ", message.getMessageText());
+                errorMessage += message.getMessageText() + "\n";
+            }
+            if( dockerServerResponseEntity.getResults()!=null){
+//                assertNotNull(response.getResults());
+          //      assertNotNull(response.getResults().getId());
+
+                String serverStatus="";
+                for (DockerServer searchDocker : dockerServerResponseEntity.getResults()) {
+                    serverID=searchDocker.getId();
+                    serverStatus= searchDocker.getDockerServerStatus()==null?"":searchDocker.getDockerServerStatus().name();
+                }
+int waitTime=0,warningCount=0;
+                provision :do{
+                    logger.info("Waiting for 10 seconds  for Docker Server to Initilize with ID [{}] ", serverID);
+                    if(maxWait<waitTime)  break provision;
+                    Thread.sleep(10000);
+                    waitTime+=10000;
+                    logger.info("Checking Serverstatus  ID [{}] ",serverID);
+                    dockerServerProvisioning=dockerServerService.findById(serverID).getResults();
+
+                    serverStatus=dockerServerProvisioning.getDockerServerStatus().name();
+                    if(serverStatus=="WARNINGS" || serverStatus.equals("CONNECTED") ) warningCount++;
+                    if(warningCount<5 && !serverStatus.equals("CONNECTED")) serverStatus="PROVISIONING";
+
+
+                }while( serverStatus=="PROVISIONING");
+                Thread.sleep(10000);
+                waitTime+=10000;
+                logger.info("Time Wait during Provisioning [{}] Minutes ",(waitTime/1000/60));
+
+                assertEquals( "CONNECTED",serverStatus);
+              /*  if(dockerServerProvisioning.getDockerServerStatus().name().equals("PROVISIONING"))
+                    dockerServerCreated=dockerServerProvisioning;*/
+
+
+            }
+
+
+
+
+
+        }
+
+       /* if (!success) {
+
+            assertNotNull(dockerServerResponseEntity.getResults());
+            assertNotNull(dockerServerCreated.getId());
 
             this.dockerServerCreated = response.getResults();
 
@@ -110,7 +174,7 @@ public class DockerServerCreateServiceTest extends AbstractServiceTest {
             assertEquals(dockerServer.getSize(), dockerServerCreated.getSize());
             assertEquals(dockerServer.getDataCenter(), dockerServerCreated.getDataCenter());
 
-        }
+        }*/
 
 
     }
@@ -120,12 +184,45 @@ public class DockerServerCreateServiceTest extends AbstractServiceTest {
         logger.info("cleaning up...");
 
         if (!success) {
-            if (dockerServerCreated != null)
-                dockerServerService.delete(dockerServerCreated.getId());
+
+            if (dockerServerProvisioning != null)
+                dockerServerService.delete(dockerServerProvisioning.getId());
+
+            //assertNotNull("Unable to Delete Object after Test ",dockerServerService.findById(dockerServerProvisioning.getId()).getResults());
+            dockerServerService.delete(dockerServerProvisioning.getId());
+            assertEquals("Unable to Delete Object after Test ",0,testRegistryAccountPosition(dockerServerProvisioning.getId()));
             if (dockerServer.getDataCenter() != null)
                 dataCenterService.delete(dockerServer.getDataCenter().getId());
 
         }
+    }
+    public int testRegistryAccountPosition(String id) {
+
+        ResponseEntity<List<DockerServer>> response = dockerServerService.findAll();
+
+        String errors = "";
+        for (Message message : response.getMessages())
+            errors += ("Error while Find All request  " + message.getMessageText() + "\n");
+
+
+        assertNotNull(response);
+        assertNotNull(response.isErrors());
+        assertThat(false, is(equals(response.isErrors())));
+        int position=0;
+        if(id!=null) {
+            for (DockerServer obj : response.getResults()) {
+
+                if(obj.getDataCenterName( ).equals("AbedeenCluster")){
+                    position=1;
+                  //  logger.info("  Object Matched in FindAll {}  at Position : {}", id, position);
+                 //   assertEquals("Recently Created Object is not at Positon 1 :"+obj.getId(),1, position);
+                }
+            }
+        }
+
+        logger.info(" Total Number of Objects :{}",response.getResults().size());
+
+        return position;
     }
 
 }
