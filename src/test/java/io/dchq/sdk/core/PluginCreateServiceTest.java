3e131af8add8272f8615ca3c5db0e2335c64b6d9
abedeen <s.z.abedeen@gmail.com>
2016-04-22 08:41:38 +0530

    Dynamic Test Case -- in Complete

--- src/test/java/io/dchq/sdk/core/PluginCreateServiceTest.java
+++ src/test/java/io/dchq/sdk/core/PluginCreateServiceTest.java
@@ -15,9 +15,7 @@
  */
 package io.dchq.sdk.core;
 
-import com.dchq.schema.beans.base.Message;
 import com.dchq.schema.beans.base.ResponseEntity;
-import com.dchq.schema.beans.one.container.Env;
 import com.dchq.schema.beans.one.plugin.Plugin;
 
 import com.dchq.schema.beans.one.security.EntitlementType;
@@ -27,23 +25,88 @@ import org.junit.FixMethodOrder;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.junit.runners.Parameterized;
-import org.springframework.util.StringUtils;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 
 /**
- * PluginService create tests
+ * Created by Abedeen on 04/05/16.
+ */
+
+/**
+ * Abstracts class for holding test credentials.
  *
- * @author Abedeen
- * @author Intesar Mohammed
+ * @author Abedeen.
  * @since 1.0
  */
+class Obj {
+
+    int[] testMaxCount;
+    int totalTestCase;
+    int caseItems;
+    int[] counter;
+    int[] move;
+    String content;
+    Object contentObjecct;
+    int loopCount = 0;
+
+    public void arrangeCounter() {
+
+        for (int i = caseItems - 2; i >= 0; i--) {
+            if (loopCount > 0) {
+                if (counter[i] + 1 < testMaxCount[i]) {
+                    loopCount = 0;
+                    counter[i]++;
+                } else if (counter[i] + 1 == testMaxCount[i]) counter[i] = 0;
+            }
+        }
+    }
+
+
+    public Obj(int[] testMaxCount, int totalTestCase, int caseItems) {
+        this.testMaxCount = testMaxCount;
+        this.totalTestCase = totalTestCase;
+        this.caseItems = caseItems;
+        counter = new int[testMaxCount.length];
+        move = new int[testMaxCount.length];
+        for (int i = 0; i < move.length; i++) {
+            if (i == move.length - 1) move[i] = 1;
+            else move[i] = 0;
+        }
+    }
+
+    public int[] getTestMaxCount() {
+        return testMaxCount;
+    }
+
+    public void setTestMaxCount(int[] testMaxCount) {
+        this.testMaxCount = testMaxCount;
+    }
+
+    public int getTotalTestCase() {
+        return totalTestCase;
+    }
+
+    public void setTotalTestCase(int totalTestCase) {
+        this.totalTestCase = totalTestCase;
+    }
+
+    public int getCaseItems() {
+        return caseItems;
+    }
+
+    public void setCaseItems(int caseItems) {
+        this.caseItems = caseItems;
+    }
+
+}
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(Parameterized.class)
 public class PluginCreateServiceTest extends AbstractServiceTest {
@@ -56,134 +119,189 @@ public class PluginCreateServiceTest extends AbstractServiceTest {
     }
 
     private Plugin plugin;
-    private boolean errors;
+    private boolean success;
     private Plugin pluginCreated;
     private String validityMessage;
 
-    /**
-     * Name: Not-Empty, Max_Length:Short-Text, Unique with Version
-     * Version: default:1.0,
-     * Description: Optional, Max_length:Large-Text
-     * Script: Not-Empty, Large-Text
-     * Script-Lang: default:SHELL, POWERSHELL, PERL, RUBY, PYTHON
-     * License: default:EUlA, Apache License 2.0
-     * Timeout: default:30, > 0, Max < ?
-     * Entitlement-Type: OWNER, CUSTOM: USERS, GROUPS
-     * Entitled-Users:
-     * Valid user_id
-     * Entitled-Groups
-     * Valid group_id
-     * Arguments: Optional
-     * ScriptArgs: Optional
-     * ENV: Optional
-     * prop:  Not-Empty
-     * val: Not-Empty
-     * eVal: value should be ignored
-     * hidden: default: false, true
-     * InActive: Empty: false, true
-     * <p/>
-     * <p/>
-     * Test-Cases
-     * 1. Valid - name, version, script,
-     */
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                // Positive Test-Cases
-                {"TestPlugin11", "1.1", null, "Dummy Script", "PERL", "Apache License 2.0", 30, EntitlementType.CUSTOM, true, userId2, null, new HashSet<>(Arrays.asList(new Env().withProp("prop1").withVal("val1"))), true, "General Input", false},
+    @Parameterized.Parameters(name = "Name: {3}")
+    public static Iterable<Object[]> generateParameters() {
 
-                // Negative Test-Cases
-        });
+        List<Object[]> governamce = new ArrayList<Object[]>();
+        governamce.add(new Object[]{"*", "*", "*", "shell,perl,python,ruby"});
+
+
+        List<Object[]> result = new ArrayList<Object[]>();
+        result.add(new Object[]{"Hero", "h1", "h2"});
+        result.add(new Object[]{"Dummy_test_Plugin", "", "121"});
+        result.add(new Object[]{"pulgin Sript"});
+        result.add(new Object[]{"shell", "perl", "python", "ruby", "", "Rails"});
+
+        result = testCaseGovernance(governamce, result);
+        return result;
     }
 
-    public PluginCreateServiceTest(String pluginName, String version, String description, String pluginScript, String scriptType, String license,
-                                   Integer timeout, EntitlementType entitlementType, boolean isEntitlementTypeUser, String entitledUserId,
-                                   String scriptArgs, Set<Env> envs, Boolean inactive,
+    public PluginCreateServiceTest(String pluginName, String pluginScript, String scriptType,
                                    String validityMessage, boolean errors) {
         this.plugin = new Plugin();
         this.plugin.setName(pluginName);
-        this.plugin.setVersion(version);
-        this.plugin.setDescription(description);
-
+        //    this.plugin.setVersion(version);
         this.plugin.setBaseScript(pluginScript);
         this.plugin.setScriptLang(scriptType);
+        //    this.plugin.setLicense(license);
 
-        this.plugin.setLicense(license);
-        this.plugin.setTimeout(timeout);
-
-        this.plugin.setEnvs(envs);
-        this.plugin.setScriptArgs(scriptArgs);
-
-        this.plugin.setEntitlementType(entitlementType);
-
-        this.plugin.setInactive(inactive);
-
-
-        this.errors = errors;
+        this.success = errors;
         this.validityMessage = validityMessage;
 
     }
 
+    public static List<Object[]> testCaseGovernance(List<Object[]> governamce, List<Object[]> sampleCase) {
+        int[] testMaxCount = getItemcount(sampleCase);
+        int testCaseCount = getItemcountFactorial(sampleCase);
+        int testCase = testMaxCount.length;
+
+        System.out.println( "Combination of Test Cases :" + testCaseCount);
+        System.out.println( "Generating Combination of test cases....");
+        int[] counter = new int[testMaxCount.length];
+        Obj calculation = new Obj(testMaxCount, testCaseCount, testCase);
+        List<Object[]> result = new ArrayList<Object[]>();
+        for (int i = 0; i < testCaseCount; i++) {
+            Object[] temptestCase = new Object[testMaxCount.length];
+            for (int j = 0; j < testCase; j++) {
+                Object[] ob = sampleCase.get(j);
+                calculation = getObjectItem(sampleCase, calculation, j);
+                temptestCase[j] = calculation.contentObjecct;
+                if (j < testCase - 1) System.out.print(" , ");
+            }
+            result.add(temptestCase);
+
+        }
+        System.out.println("Total Number of for Test Cases : "+result.size());
+        System.out.println("Generating Governance for Test Cases");
+        for(int i=0;i<governamce.size();i++)
+        {
+            Object[] govCase =governamce.get(i);
+            for(int j=0;j<result.size();j++)
+            {
+                Object[] singleCase =result.get(i);
+                singleCase=getExpectedResult(govCase,singleCase);
+
+            }
+
+
+        }
+        return result;
+    }
+
+    public static  Object[] getExpectedResult(Object[] govCase,Object[] singlecase)
+    {
+        Object[] expectTestResult= new Object[singlecase.length+1];
+
+        return null;
+    }
     @org.junit.Test
+    public void testDummyCreate() throws Exception {
+
+
+    }
+
+    public static Obj getItem(List<Object[]> sampleCase, Obj sc, int pos) {
+
+        Object[] test = sampleCase.get(pos);
+
+        int nextCount = sc.counter[pos];
+        sc.content = test[nextCount].toString();
+        if (sc.counter[pos] == sc.testMaxCount[pos] - 1 && sc.move[pos] == 1) {
+            sc.counter[pos] = 0;
+            sc.loopCount++;
+            sc.arrangeCounter();
+        } else if (sc.move[pos] == 1)
+            sc.counter[pos]++;
+        return sc;
+    }
+
+    public static Obj getObjectItem(List<Object[]> sampleCase, Obj sc, int pos) {
+
+        Object[] test = sampleCase.get(pos);
+
+        int nextCount = sc.counter[pos];
+        sc.contentObjecct = test[nextCount];
+        if (sc.counter[pos] == sc.testMaxCount[pos] - 1 && sc.move[pos] == 1) {
+            sc.counter[pos] = 0;
+            sc.loopCount++;
+            sc.arrangeCounter();
+        } else if (sc.move[pos] == 1)
+            sc.counter[pos]++;
+        return sc;
+    }
+
+    public static int[] getItemcount(List<Object[]> sampleCase) {
+        Object[] ob = sampleCase.get(0);
+        String itemCount = "";
+        int count = 1;
+        for (int i = 0; i < sampleCase.size(); i++) {
+            Object[] obj = sampleCase.get(i);
+            itemCount += obj.length + ",";
+        }
+        String[] numberStrs = itemCount.split(",");
+        int[] numbers = new int[numberStrs.length];
+        for (int i = 0; i < numberStrs.length; i++)
+            numbers[i] = Integer.parseInt(numberStrs[i]);
+
+        return numbers;
+    }
+
+    public static int getItemcountFactorial(List<Object[]> sampleCase) {
+        Object[] ob = sampleCase.get(0);
+        String itemCount = "";
+        int count = 1;
+        for (int i = 0; i < sampleCase.size(); i++) {
+            Object[] obj = sampleCase.get(i);
+            itemCount += obj.length + ",";
+        }
+        String[] numberStrs = itemCount.split(",");
+        int numbers = 1;
+        for (int i = 0; i < numberStrs.length; i++)
+            numbers *= Integer.parseInt(numberStrs[i]);
+
+        return numbers;
+    }
+
+    public static int getTestCaseCount(int number) {
+        int factorial = number;
+
+        for (int i = (number - 1); i > 1; i--) {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+
+    //  @org.junit.Test
     public void testCreate() throws Exception {
 
-        logger.info("Creating Plugin with name [{}]", this.plugin.getName());
+        logger.info("Create Group with Group Name [{}]", this.plugin.getName());
         ResponseEntity<Plugin> response = appService.create(plugin);
 
-        if (response.isErrors()) {
-            for (Message m : response.getMessages())
-                logger.warn("[{}]", m.getMessageText());
-        }
+        if (response.isErrors())
+            logger.warn("Message from Server... {}", response.getMessages().get(0).getMessageText());
 
         assertNotNull(response);
         assertNotNull(response.isErrors());
 
-        assertThat(errors, is(equals(response.isErrors())));
+        Assert.assertNotNull(((Boolean) success).toString(), ((Boolean) response.isErrors()).toString());
 
-        if (!response.isErrors()) {
+        if (!response.isErrors() && response.getResults() != null) {
             pluginCreated = response.getResults();
             assertNotNull(response.getResults());
             assertNotNull(response.getResults().getId());
 
-            // Comparing objects against created Objects.
-            assertEquals(plugin.getName(), pluginCreated.getName());
-
-            // handle empty
-            if (StringUtils.isEmpty(plugin.getVersion())) {
-                assertEquals("1.0", pluginCreated.getVersion());
-            } else {
-                assertEquals(plugin.getVersion(), pluginCreated.getVersion());
-            }
-
-            assertEquals(plugin.getDescription(), pluginCreated.getDescription());
-
-            // handle empty
-            if (StringUtils.isEmpty(plugin.getLicense())) {
-                assertEquals("EULA", pluginCreated.getLicense());
-            } else {
-                assertEquals(plugin.getLicense(), pluginCreated.getLicense());
-            }
-
-            // handle empty
-            if (StringUtils.isEmpty(plugin.getTimeout())) {
-                assertEquals("30", pluginCreated.getTimeout());
-            } else {
-                assertEquals(plugin.getTimeout(), pluginCreated.getTimeout());
-            }
-
-            assertEquals(plugin.getBaseScript(), pluginCreated.getBaseScript());
-            // handle empty
-            if (StringUtils.isEmpty(plugin.getScriptLang())) {
-                assertEquals("SHELL", pluginCreated.getScriptLang());
-            } else {
-                assertEquals(plugin.getScriptLang(), pluginCreated.getScriptLang());
-            }
-            assertEquals(plugin.getEnvs(), pluginCreated.getEnvs());
-            assertEquals(plugin.getScriptArgs(), pluginCreated.getScriptArgs());
-            // TODO handle empty
-            //assertEquals(plugin.getInactive(), pluginCreated.getInactive());
-
-            assertEquals(plugin.getEntitlementType(), pluginCreated.getEntitlementType());
+            // Comparing objects agains created Objects.
+            Assert.assertNotNull(plugin.getName(), pluginCreated.getName());
+            Assert.assertNotNull(plugin.getName(), pluginCreated.getName());
+            Assert.assertNotNull(plugin.getVersion(), pluginCreated.getVersion());
+            Assert.assertNotNull(plugin.getBaseScript(), pluginCreated.getBaseScript());
+            Assert.assertNotNull(plugin.getScriptLang(), pluginCreated.getScriptLang());
+            Assert.assertNotNull(plugin.getLicense(), pluginCreated.getLicense());
+            Assert.assertNotNull(plugin.getEntitlementType().toString(), pluginCreated.getEntitlementType().toString());
 
         }
 
