e63d0d6d3281e29c0c88a7300a49407117c42b45
Intessar Mohammed <mdshannan@gmail.com>
2017-01-23 14:00:40 -0800

    TenantService, UsersCreateServiceTest fixed.

---
+++ src/main/java/io/dchq/sdk/core/TenantServiceImpl.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.dchq.sdk.core;
+
+import com.dchq.schema.beans.base.ResponseEntity;
+import com.dchq.schema.beans.one.security.Tenant;
+import org.springframework.core.ParameterizedTypeReference;
+
+import java.util.List;
+
+/**
+ * Encapsulates DCHQ Tenant calls.
+ *
+ * @author Intesar Mohammed
+ * @see <a href="https://dchq.readme.io/docs/tenant-1">Tenant endpoint</a>
+ * @since 1.0
+ */
+public class TenantServiceImpl extends GenericServiceImpl<Tenant, ResponseEntity<List<Tenant>>, ResponseEntity<Tenant>>
+        implements TenantService {
+
+    public static final ParameterizedTypeReference<ResponseEntity<List<Tenant>>> listTypeReference = new ParameterizedTypeReference<ResponseEntity<List<Tenant>>>() {
+    };
+    public static final ParameterizedTypeReference<ResponseEntity<Tenant>> singleTypeReference = new ParameterizedTypeReference<ResponseEntity<Tenant>>() {
+    };
+
+    public static final String ENDPOINT = "tenant/";
+
+    /**
+     * @param baseURI  - e.g. https://dchq.io/api/1.0/
+     * @param username - registered username with DCHQ.io
+     * @param password - password used with the username
+     */
+    public TenantServiceImpl(String baseURI, String username, String password) {
+        super(baseURI, ENDPOINT, username, password,
+                new ParameterizedTypeReference<ResponseEntity<List<Tenant>>>() {
+                },
+                new ParameterizedTypeReference<ResponseEntity<Tenant>>() {
+                }
+        );
+    }
+
+}
--- src/test/java/io/dchq/sdk/core/users/UsersCreateServiceTest.java
+++ src/test/java/io/dchq/sdk/core/users/UsersCreateServiceTest.java
@@ -16,44 +16,43 @@
 
 package io.dchq.sdk.core.users;
 
-import static junit.framework.TestCase.assertNotNull;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-
-import org.junit.After;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
-import org.junit.runners.Parameterized;
-
 import com.dchq.schema.beans.base.Message;
 import com.dchq.schema.beans.base.ResponseEntity;
 import com.dchq.schema.beans.one.base.PkEntityBase;
 import com.dchq.schema.beans.one.security.Organization;
 import com.dchq.schema.beans.one.security.Profile;
 import com.dchq.schema.beans.one.security.Users;
-
 import io.dchq.sdk.core.AbstractServiceTest;
 import io.dchq.sdk.core.ProfileSearchServiceTest;
 import io.dchq.sdk.core.ServiceFactory;
 import io.dchq.sdk.core.UserService;
 import io.dchq.sdk.core.clusters.DataCenterSearchServiceTest;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 
 /**
  * <code>UsersService</code> Integration Tests.
  *
  * @author Intesar Mohammed
  * @since 1.0
- *        <p/>
- *        Users: Create (annonymous & ENABLED, ROLE_TENANT_ADMIN)
+ * <p/>
+ * Users: Create (annonymous & ENABLED, ROLE_TENANT_ADMIN)
  */
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -62,43 +61,42 @@ import io.dchq.sdk.core.clusters.DataCenterSearchServiceTest;
 public class UsersCreateServiceTest extends AbstractServiceTest {
 
     private UserService service;
-	private Users users;
-	private boolean error;
-	private Users userCreated;
-	private String errorMessage;
+    private Users users;
+    private boolean error;
+    private Users userCreated;
+    private String errorMessage;
 
-	@org.junit.Before
-	public void setUp() throws Exception {
-		service = ServiceFactory.buildUserService(rootUrl, username, password);
-	}
+    @org.junit.Before
+    public void setUp() throws Exception {
+        service = ServiceFactory.buildUserService(rootUrl, username, password);
+    }
 
-	public static PkEntityBase searchDataCenter(String term) throws Exception {
-		DataCenterSearchServiceTest dsst = new DataCenterSearchServiceTest();
-		PkEntityBase pkeb = dsst.searchDataCenter(term);
-		return pkeb;
-	}
+    public static PkEntityBase searchDataCenter(String term) throws Exception {
+        DataCenterSearchServiceTest dsst = new DataCenterSearchServiceTest();
+        PkEntityBase pkeb = dsst.searchDataCenter(term);
+        return pkeb;
+    }
 
-	public static Profile getProfile(String name) {
-		try {
-			ProfileSearchServiceTest tempProf = new ProfileSearchServiceTest();
-			return tempProf.searchProfile(name);
-		} catch (Exception e) {
-			System.out.println("ERROR :" + e.getMessage());
-		}
-		return null;
-	}
+    public static Profile getProfile(String name) {
+        try {
+            ProfileSearchServiceTest tempProf = new ProfileSearchServiceTest();
+            return tempProf.searchProfile(name);
+        } catch (Exception e) {
+            System.out.println("ERROR :" + e.getMessage());
+        }
+        return null;
+    }
 
-	public static Organization getOrganization(String name, Boolean inActive, Boolean deleted) {
-		return new Organization().withName(name).withInactive(inActive).withDeleted(deleted);
-	}
+    public static Organization getOrganization(String name, Boolean inActive, Boolean deleted) {
+        return new Organization().withName(name).withInactive(inActive).withDeleted(deleted);
+    }
 
-	@Parameterized.Parameters
-	public static Collection<Object[]> data() throws Exception {
-		int userId = getRandomUserId();
-		return Arrays.asList(new Object[][] {
-			   
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() throws Exception {
+        return Arrays.asList(new Object[][]{
+
 				/* { null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, "All Null", true }
-				,{ null, null, "", "a@b.com", null, null, null, null, null, null, null, null, "", null, null, "Only Username", false }
+                ,{ null, null, "", "a@b.com", null, null, null, null, null, null, null, null, "", null, null, "Only Username", false }
 				,{ null, "Last", "admin", null, null, null, null, null, null, null, null, null, null, null, null, "Only ,ln UserName", true }
 				,{ null, "Last", "testUser1", null, null, null, null, null, null, null, null, null, null, null, null, "Only ln,Username", true }
 				,{ null, "Last", "testuser1", "it-user@dchq.io", null, null, null, null, null, null, null, null, null, null, null, "Only ln,Username,emailid,", true } 
@@ -119,118 +117,112 @@ public class UsersCreateServiceTest extends AbstractServiceTest {
 				,{ "fn", "ln", "ituser1", "ituser3@dchq.io", "", "System Creating User with Empty Password,\n SDK Malfunction :Creating user with Empty Password", true }
 				,{ "", "", "ituser2", "ituser4@dchq.io", "", false }
 				 */
-				{ "fname", "lname", "user"+userId ,  "user"+userId+"@dchq.io" , "ABC", "Engg", "123-1231-121",
-						getProfile("BASIC"), null, null, null, null, "pass1234", null, null, "comments", false }				
-		});
-	}
+                {"fname", "lname", "user", "user" + "@dchq.io", "ABC", "Engg", "123-1231-121",
+                        getProfile("BASIC"), null, null, null, null, "pass1234", null, null, "comments", false}
+        });
+    }
 
-	public UsersCreateServiceTest(
-			String fn, 
-			String ln, 
-			String username, 
-			String email, 
-			String company, 
-			String title,
-			String phoneNumber, 
-			Profile profile, 
-			String tenant, 
-			Organization organization, 
-			Set<String> userGroupId,
-			List<String> authorities, 
-			String pass, 
-			PkEntityBase cluster, 
-			Boolean isActive, 
-			String message,
-			boolean success
-	) 
-	{
-		this.users = new Users().withFirstname(fn).withLastname(ln).withUsername(username).withEmail(email).withPassword(pass);
-		this.users.setCompany(company);
-		this.users.setJobTitle(title);
-		this.users.setPhoneNumber(phoneNumber);
-		this.users.setProfile(profile);
-		this.users.setTenantPk(tenant);
-		this.users.setInactive(false);
-		this.users.setOrganization(organization);
-		this.users.setUserGroupIds(userGroupId);
-		this.users.setAuthorities(authorities);
-		this.users.setPreferredDataCenter(cluster);
-		this.users.setInactive(isActive);
-		this.errorMessage = message;
-		this.error = success;
-	};
+    public UsersCreateServiceTest(
+            String fn,
+            String ln,
+            String username,
+            String email,
+            String company,
+            String title,
+            String phoneNumber,
+            Profile profile,
+            String tenant,
+            Organization organization,
+            Set<String> userGroupId,
+            List<String> authorities,
+            String pass,
+            PkEntityBase cluster,
+            Boolean isActive,
+            String message,
+            boolean success
+    ) {
+
+        // random username
+        String prefix = RandomStringUtils.randomAlphabetic(3);
+        username = prefix + "-" + username;
+        email = prefix + "-" + email;
+
+        // lowercase
+        username = org.apache.commons.lang3.StringUtils.lowerCase(username);
+        email = org.apache.commons.lang3.StringUtils.lowerCase(email);
+
+        this.users = new Users().withFirstname(fn).withLastname(ln).withUsername(username).withEmail(email).withPassword(pass);
+        this.users.setCompany(company);
+        this.users.setJobTitle(title);
+        this.users.setPhoneNumber(phoneNumber);
+        //this.users.setProfile(profile);
+        this.users.setTenantPk(tenant);
+        this.users.setInactive(false);
+        //this.users.setOrganization(organization);
+        //this.users.setUserGroupIds(userGroupId);
+        this.users.setAuthorities(authorities);
+        //this.users.setPreferredDataCenter(cluster);
+        this.users.setInactive(isActive);
+        this.errorMessage = message;
+        this.error = success;
+    }
+
+    ;
 
     @Test
-	public void testCreate() {
-		logger.info("Create user fn [{}] ln [{}] username [{}]", users.getFirstname(), users.getLastname(), users.getUsername());
-		ResponseEntity<Users> response = service.create(users);
-		
-		for (Message message : response.getMessages())
-			logger.warn("Error while Create request  [{}] ", message.getMessageText());
-		if (response.getResults() != null) {
-			this.userCreated = response.getResults();
-			logger.info("Create user Successful..");
-		}
-		// check response is not null
-		// check response has no errors50zc
-		// check response has user entity with ID
-		// check all data send
-		assertNotNull(response);
-		assertNotNull(response.isErrors());
-		// assertEquals("Expected :\n" + errorMessage, error, response.isErrors());
-		if (!error) {
-			assertNotNull(response.getResults());
-			assertNotNull(response.getResults().getId());
-			assertEquals(users.getFirstname(), userCreated.getFirstname());
-			assertEquals(users.getLastname(), userCreated.getLastname());
-			assertEquals(users.getUsername() , userCreated.getUsername());
-			assertEquals(users.getEmail(), userCreated.getEmail());
-			assertEquals(users.getCompany(), userCreated.getCompany());
-			assertEquals(users.getJobTitle(), userCreated.getJobTitle());
-			assertEquals(users.getProfile(), userCreated.getProfile());
-			assertEquals(users.getPhoneNumber(), userCreated.getPhoneNumber());
-			assertEquals(users.getPhoneNumber(), userCreated.getPhoneNumber());
-			assertEquals(users.getPhoneNumber(), userCreated.getPhoneNumber());
-			assertEquals(users.getPhoneNumber(), userCreated.getPhoneNumber());
-			assertEquals(users.getPhoneNumber(), userCreated.getPhoneNumber());
-			assertEquals(users.getPhoneNumber(), userCreated.getPhoneNumber());
-			// assertEquals(("ORG_ZERO"),userCreated.getOrganization().getName());
-			assertEquals(users.getUserGroupIds(), userCreated.getUserGroupIds());
-			assertEquals(users.getAuthorities(), userCreated.getAuthorities());
+    public void testCreate() {
+        logger.info("Create user fn [{}] ln [{}] username [{}]", users.getFirstname(), users.getLastname(), users.getUsername());
+        ResponseEntity<Users> response = service.create(users);
 
-			if (isNullOrEmpty(users.getUserGroupIds()) && users.getUserGroups() == null)
-				assertNull(userCreated.getUserGroups());
+        for (Message message : response.getMessages()) {
+            logger.warn("Error while Create request  [{}] ", message.getMessageText());
+        }
+        if (response.getResults() != null) {
+            this.userCreated = response.getResults();
+            logger.info("Create user Successful..");
+        }
+        // check response is not null
+        // check response has no errors50zc
+        // check response has user entity with ID
+        // check all data send
+        assertNotNull(response);
+        assertNotNull(response.isErrors());
+        // assertEquals("Expected :\n" + errorMessage, error, response.isErrors());
+        if (!error) {
+            assertNotNull(response.getResults());
+            assertNotNull(response.getResults().getId());
+            assertEquals(users.getFirstname(), userCreated.getFirstname());
+            assertEquals(users.getLastname(), userCreated.getLastname());
+            assertEquals(users.getUsername(), userCreated.getUsername());
+            assertEquals(users.getEmail(), userCreated.getEmail());
+            assertEquals(users.getCompany(), userCreated.getCompany());
+            assertEquals(users.getJobTitle(), userCreated.getJobTitle());
+            assertEquals(users.getProfile(), userCreated.getProfile());
+            assertEquals(users.getPhoneNumber(), userCreated.getPhoneNumber());
+            assertEquals(users.getAuthorities(), userCreated.getAuthorities());
 
-			if (!isNullOrEmpty(users.getUserGroupIds())) {
-				assertNotNull(userCreated.getUserGroups());
-			}
 
-			if (!isNullOrEmpty(users.getUserGroups())) {
-				assertNotNull(userCreated.getUserGroups());
-			}
+            if (isNullOrEmpty(users.getTenantPk()))
+                assertNotNull(userCreated.getTenantPk());
+            else
+                assertEquals(users.getTenantPk(), userCreated.getTenantPk());
 
-			if (!isNullOrEmpty(users.getPreferredDataCenter()))
-				assertEquals(users.getPreferredDataCenter().getId(), userCreated.getPreferredDataCenter().getId());
-			else
-				assertNull(userCreated.getPreferredDataCenter());
+            // Password should always be empty
+            assertThat("", is(response.getResults().getPassword()));
+        }
+    }
 
-			if (isNullOrEmpty(users.getTenantPk()))
-				assertNotNull(userCreated.getTenantPk());
-			else
-				assertEquals(users.getTenantPk(), userCreated.getTenantPk());
-
-			// Password should always be empty
-			assertThat("", is(response.getResults().getPassword()));
-		}
-    };
+    ;
 
     @After
     public void cleanUp() {
-        if (userCreated!=null) {
+        if (userCreated != null) {
             logger.info("cleaning up...");
-            service.delete(userCreated.getId());
+            ResponseEntity<?> response = service.delete(userCreated.getId());
+            for (Message message : response.getMessages()) {
+                logger.warn("Error user deletion: [{}] ", message.getMessageText());
+            }
         }
-    };
-
+    }
 
 }
