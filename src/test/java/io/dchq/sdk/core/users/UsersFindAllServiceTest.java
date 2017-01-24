a66e16a0f20b56cf145223853b77c2c99e31367d
jagdeepjain <jagdeep.jain@gmail.com>
2017-01-24 12:59:46 +0530

    added assert for getAllElements after findAll(page, size) call

--- src/test/java/io/dchq/sdk/core/users/UsersFindAllServiceTest.java
+++ src/test/java/io/dchq/sdk/core/users/UsersFindAllServiceTest.java
@@ -27,6 +27,7 @@ import java.util.List;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -96,6 +97,7 @@ public class UsersFindAllServiceTest extends AbstractServiceTest {
 
 	public int testGetUserFromFindAll(String id) {
 		ResponseEntity<List<Users>> response = service.findAll(0, 5000);
+		Assert.assertNotNull(response.getTotalElements());
 		String errors = "";
 		for (Message message : response.getMessages()) {
 			errors += ("Error while Find request  " + message.getMessageText() + "\n");
